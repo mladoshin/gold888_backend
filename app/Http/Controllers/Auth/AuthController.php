ca19cba743928b888c962c975bf68a676efa930a
abdurauf96 <saydaliyevabdurauf@mail.ru>
2024-05-29 15:24:45 +0500

    login api

---
+++ app/Http/Controllers/Auth/AuthController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Http\Request;
+use Illuminate\Validation\ValidationException;
+
+class AuthController extends Controller
+{
+    public function login(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|string|email',
+            'password' => 'required|string',
+        ]);
+
+        $user = User::where('email', $request->email)->first();
+
+        if (!$user || !Hash::check($request->password, $user->password)) {
+            throw ValidationException::withMessages([
+                'email' => ['The provided credentials are incorrect.'],
+            ]);
+        }
+
+        return response()->json(['success' => true, 'data' => ['token' => $user->createToken('Laravel')->plainTextToken]]);
+    }
+}
