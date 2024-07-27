b4b5aaa63d59b637f569d659991db57a00d08a1d
abdurauf96 <saydaliyevabdurauf@mail.ru>
2024-07-27 10:56:37 +0500

    regions api crud

---
+++ app/Http/Controllers/Api/RegionController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Resources\RegionResource;
+use App\Models\Region;
+use Illuminate\Http\Request;
+
+class RegionController extends Controller
+{
+    public function index()
+    {
+        return RegionResource::collection(Region::latest()->paginate(10));
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate(['name' => 'required']);
+        $region = Region::create($request->all());
+        return new RegionResource($region);
+    }
+
+    public function show(Region $region)
+    {
+        return new RegionResource($region);
+    }
+
+    public function update(Request $request, Region $region)
+    {
+        $request->validate(['name' => 'required']);
+        $region->update($request->all());
+        return new RegionResource($region);
+    }
+
+    public function destroy(Region $region)
+    {
+        $region->delete();
+        return response()->json(['message' => 'region deleted']);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\RegionController;
 use App\Http\Controllers\Auth\AuthController;
 use App\Http\Controllers\Api\ReportController;
 use Illuminate\Http\Request;
@@ -21,16 +22,22 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::middleware('auth:sanctum')->group(function (){
+
+    Route::apiResource('regions',RegionController::class);
+
+
+    //reports
+    Route::get('reports/last',[ReportController::class, 'getLastReport']);
+    Route::apiResource('reports',ReportController::class);
+    Route::get('reports/statistics',[ReportController::class, 'statistics']);
+
+
     Route::post('auth/logout', [AuthController::class, 'logout']);
     Route::get('check-auth',function (){
         if (!auth()->check())
             return response()->json(['success' => false, 'data' => false]);
-
         return response()->json(['success' => true, 'data' => true]);
     });
-    Route::get('reports/last',[ReportController::class, 'getLastReport']);
-    Route::apiResource('reports',ReportController::class);
-    Route::get('reports/statistics',[ReportController::class, 'statisti']);
 });
 Route::post('auth/login', [AuthController::class, 'login']);
 
